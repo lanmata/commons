b330bb2d321b0119d037e4494b1f2db8e48cc175
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-12-26 03:35:31 -0500

    DS-31 Add Common components for PRX Projects to Commons Project
    - Added unit test cases

---
+++ src/test/java/com/prx/commons/constants/keys/AuthKeyTest.java
@@ -0,0 +1,16 @@
+package com.prx.commons.constants.keys;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class AuthKeyTest {
+
+    @Test
+    void keys() {
+        for (final var authKey : AuthKey.values()) {
+            Assertions.assertTrue(Arrays.asList(AuthKey.values()).contains(authKey));
+        }
+    }
+}
